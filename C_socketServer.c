c124818fa9b820ab01024b7774a44718fef3d477
Jay Lin <jay101630@gmail.com>
2018-06-15 17:17:09 +0800

    update readme, fix c code style

--- C_socketServer.c
+++ C_socketServer.c
@@ -1,23 +1,21 @@
-#include<time.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<signal.h>
-#include<errno.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
 #include <netdb.h>
-#include<unistd.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<sys/wait.h>
-#include<sys/select.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/select.h>
 
 #define SA struct sockaddr
 #define MAXLEN 1024
-
-
 
 struct pi
 {
@@ -29,7 +27,7 @@ struct pi
     FILE *file;
 };
 
-int main(int argc,char**argv)
+int main(int argc, char **argv)
 {
     int i;
     int ser_port;
@@ -44,171 +42,178 @@ int main(int argc,char**argv)
     struct sockaddr_in servaddr;
     struct sockaddr_in cliaddr;
     FILE *nowLog;
-    fd_set rset,iniset;
-    nowClientNum=0;
-    if(argc !=2)
+    fd_set rset, iniset;
+    nowClientNum = 0;
+    if (argc != 2)
     {
-        fprintf(stderr,"Useage: ./C_socketServer <port>\n");
+        fprintf(stderr, "Useage: ./C_socketServer <port>\n");
         exit(-1);
     }
-    ser_port=atoi(argv[1]);
-    s_lisfd=socket(AF_INET,SOCK_STREAM,0);
-    if(s_lisfd<0)
+    ser_port = atoi(argv[1]);
+    s_lisfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (s_lisfd < 0)
     {
-        fprintf(stderr,"Bulid listening socket error\n");
-        exit(-1);
-    }
-
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_port=htons(ser_port);
-    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-
-    if(bind(s_lisfd,(SA*)&servaddr,sizeof(servaddr))==-1)
-    {
-        fprintf(stderr,"Bind socket error\n");
+        fprintf(stderr, "Bulid listening socket error\n");
         exit(-1);
     }
 
-    if(listen(s_lisfd,10)==-1)
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(ser_port);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(s_lisfd, (SA *)&servaddr, sizeof(servaddr)) == -1)
     {
-        fprintf(stderr,"Listen error\n");
+        fprintf(stderr, "Bind socket error\n");
         exit(-1);
     }
 
-    maxfd=s_lisfd;
-    maxindex=-1;
-    for(i=0;i<FD_SETSIZE;++i)client[i].socket=-1;
+    if (listen(s_lisfd, 10) == -1)
+    {
+        fprintf(stderr, "Listen error\n");
+        exit(-1);
+    }
+
+    maxfd = s_lisfd;
+    maxindex = -1;
+    for (i = 0; i < FD_SETSIZE; ++i)
+        client[i].socket = -1;
     FD_ZERO(&iniset);
-    FD_SET(s_lisfd,&iniset);
+    FD_SET(s_lisfd, &iniset);
 
-    for(;;)
+    for (;;)
     {
         int nready;
-        rset=iniset;
-        if((nready=select(maxfd+1,&rset,NULL,NULL,NULL))<0)
+        rset = iniset;
+        if ((nready = select(maxfd + 1, &rset, NULL, NULL, NULL)) < 0)
         {
-            fprintf(stderr,"select wrong\n");
+            fprintf(stderr, "select wrong\n");
             exit(-1);
         }
 
-        if(FD_ISSET(s_lisfd,&rset))
+        if (FD_ISSET(s_lisfd, &rset))
         {
-            socklen_t clilen=sizeof(cliaddr);
-            if((s_confd=accept(s_lisfd,(SA*)&cliaddr,&clilen))==-1)
+            socklen_t clilen = sizeof(cliaddr);
+            if ((s_confd = accept(s_lisfd, (SA *)&cliaddr, &clilen)) == -1)
             {
-                fprintf(stderr,"accept wrong\n");
+                fprintf(stderr, "accept wrong\n");
                 exit(-1);
             }
-            for(i=0;i<FD_SETSIZE;++i)
+            for (i = 0; i < FD_SETSIZE; ++i)
             {
-                if(client[i].socket>0)continue;
+                if (client[i].socket > 0)
+                    continue;
                 char *str;
                 int PIid;
                 time_t rawtime;
-                client[i].socket=s_confd;
+                client[i].socket = s_confd;
                 nowClientNum++;
                 printf("One beacon scanner connect to server!\n");
-                read(client[i].socket,recevBuffer,MAXLEN);
-                str=strtok(recevBuffer," \n");
-                PIid=atoi(str);
-                client[i].piID=PIid;
-                printf("id is %d %d\n",PIid,i);
+                read(client[i].socket, recevBuffer, MAXLEN);
+                str = strtok(recevBuffer, " \n");
+                PIid = atoi(str);
+                client[i].piID = PIid;
+                printf("id is %d %d\n", PIid, i);
                 printf("start to write log file\n");
                 printf("------------------------\n");
-                snprintf(client[i].LogFile,40,"%d_beacon_scannerlog.txt",i);
-                client[i].file=fopen(client[i].LogFile,"w");
-                time (&rawtime);
-                fprintf(client[i].file,"Connect at %s", ctime (&rawtime));
+                snprintf(client[i].LogFile, 40, "%d_beacon_scannerlog.txt", i);
+                client[i].file = fopen(client[i].LogFile, "w");
+                time(&rawtime);
+                fprintf(client[i].file, "Connect at %s", ctime(&rawtime));
                 fclose(client[i].file);
                 break;
             }
-            if(i==FD_SETSIZE)
+            if (i == FD_SETSIZE)
             {
-                fprintf(stderr,"too many clients\n");
+                fprintf(stderr, "too many clients\n");
                 exit(-1);
             }
-            FD_SET(s_confd,&iniset);
-            if(s_confd>maxfd)maxfd=s_confd;
-            if(i>maxindex)maxindex=i;
-            if(--nready<=0)continue;
+            FD_SET(s_confd, &iniset);
+            if (s_confd > maxfd)
+                maxfd = s_confd;
+            if (i > maxindex)
+                maxindex = i;
+            if (--nready <= 0)
+                continue;
         }
         int sockfd;
-        for(i=0;i<=maxindex;++i)
+        for (i = 0; i <= maxindex; ++i)
         {
-            if((sockfd=client[i].socket)<0)continue;
-            if(FD_ISSET(sockfd,&rset))
+            if ((sockfd = client[i].socket) < 0)
+                continue;
+            if (FD_ISSET(sockfd, &rset))
             {
                 ssize_t n;
-                if((n=read(sockfd,recevBuffer,MAXLEN))==0)
+                if ((n = read(sockfd, recevBuffer, MAXLEN)) == 0)
                 {
 
-                    printf("%d beacon scanner leave\n",client[i].piID);
+                    printf("%d beacon scanner leave\n", client[i].piID);
                     printf("close log file\n");
                     printf("------------------------\n");
-                    client[i].socket=-1;
-                    if(i==maxindex)
+                    client[i].socket = -1;
+                    if (i == maxindex)
                     {
                         int j;
-                        for(j=maxindex;j>=0;--j)
+                        for (j = maxindex; j >= 0; --j)
                         {
-                            if(client[j].socket>0)
+                            if (client[j].socket > 0)
                             {
-                                maxindex=j;
+                                maxindex = j;
                                 break;
                             }
                         }
-                        if(j==-1)maxindex=-1;
+                        if (j == -1)
+                            maxindex = -1;
                     }
                     nowClientNum--;
                     close(sockfd);
-                    FD_CLR(sockfd,&iniset);
+                    FD_CLR(sockfd, &iniset);
                 }
                 else
                 {
                     char *str;
                     int j;
                     int k;
-                    int nowPeople=0;
+                    int nowPeople = 0;
                     time_t rawtime;
-                    client[i].file=fopen(client[i].LogFile,"a");
-                    recevBuffer[n]=0;
-                    str=strtok(recevBuffer," \n");
-                    client[i].cur_people=atoi(str);
-                    nowLog=fopen("scan_result.log","w");
-                    for(j=0;j<client[i].cur_people;j++)
+                    client[i].file = fopen(client[i].LogFile, "a");
+                    recevBuffer[n] = 0;
+                    str = strtok(recevBuffer, " \n");
+                    client[i].cur_people = atoi(str);
+                    nowLog = fopen("scan_result.log", "w");
+                    for (j = 0; j < client[i].cur_people; j++)
                     {
-                        str=strtok(NULL," \n");
-                        strcpy(client[i].uuidList[j],str);
+                        str = strtok(NULL, " \n");
+                        strcpy(client[i].uuidList[j], str);
                     }
 
-                    for(k=0;k<=maxindex;k++)
+                    for (k = 0; k <= maxindex; k++)
                     {
-                        if(i==k)continue;
+                        if (i == k)
+                            continue;
                         //printf("%d\n",client[k].cur_people);
-                        for(j=0;j<client[i].cur_people;j++)
+                        for (j = 0; j < client[i].cur_people; j++)
                         {
 
                             int p;
-                            for(p=0;p<client[k].cur_people;p++)
+                            for (p = 0; p < client[k].cur_people; p++)
                             {
-                                if(strcmp(client[i].uuidList[j],client[k].uuidList[p])==0)nowPeople++;
+                                if (strcmp(client[i].uuidList[j], client[k].uuidList[p]) == 0)
+                                    nowPeople++;
                             }
                         }
                         break;
                     }
-                    fprintf(nowLog,"%d\n",nowPeople);
+                    fprintf(nowLog, "%d\n", nowPeople);
                     fclose(nowLog);
-                    time (&rawtime);
-                    printf("current people num :%d \n",nowPeople);
-                    fprintf(client[i].file,"Current %d people at %s",nowPeople,ctime (&rawtime));
+                    time(&rawtime);
+                    printf("current people num :%d \n", nowPeople);
+                    fprintf(client[i].file, "Current %d people at %s", nowPeople, ctime(&rawtime));
                     fclose(client[i].file);
                 }
-                if(--nready<=0)break;
+                if (--nready <= 0)
+                    break;
             }
-
         }
-
     }
-
 }
--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 > use [beacontools](https://github.com/citruz/beacontools.git) to scan ibeacon message
 
+## Environment
+- You must have python3 on your Pi
+
+- install [beacontools[scan]](https://github.com/citruz/beacontools.git) on Pi
+```sh
+# install libbluetooth headers and libpcap2
+sudo apt-get install python-dev libbluetooth-dev libcap2-bin
+
+# grant the python executable permission to access raw socket data
+sudo setcap 'cap_net_raw,cap_net_admin+eip' $(readlink -f $(which python3))
+
+# install beacontools with scanning support
+pip3 install beacontools[scan]
+```
+
 ## Build
 ```
 make
@@ -9,16 +24,40 @@ make
 this will help you compile a socket server `C_socketServer`
 
 ## RUN
-- first, run `C_socketServer` to create a tcp server
+1. first, run `C_socketServer` to create a tcp server
 ```
 ./C_socketServer <port>
 ```
+- 參數說明
+    - port: tcp server 要 listen 的 port number
 
-- then run scanner on pi to scan beacon message
+2. run scanner on pi to scan beacon message
 ```
-sudo python3 scanner.py <id> <server ip address> <port>
+sudo python3 scanner.py <id> <ip> <port>
+```
+- 參數說明
+    - id: 一個大於 0 的整數，表示樹梅派的編號
+    - ip: tcp server 的 ip address
+    - port: tcp server 的 port number
+
+## 功能說明
+- Pi 會檢查收到的 beacon 的 uuid 是否合法，每隔 2 秒將收到的合法卡號回傳給 server
+
+- server 根據 2 台不同的 Pi 回傳的合法卡號，計算位於兩台Pi beacon 掃描交集範圍的合法卡號數量，並將結果存在 `scan_result.log` 中
+
+#### scan_result.log 範例
+
+```
+2
 ```
 
-argument `id` is a nonnegative integer that help server recognize different raspi client
+表示目前有兩個合法卡號 (兩個人) 位於兩台 Pi 的交集範圍內
 
-Server 會輸出3個 log 檔，兩個是紀錄2邊 pi 傳過來的beacon數量，1個是統整之後計算出在交集範圍內的合法 beacon 數，檔名為 scan_result.log。讀 scan_result.log 即可得知目前位在2個 pi beacon掃描交集範圍內的合法 beacon 數量
+#### 合法卡號清單 範例 (uuid.txt)
+
+```bash
+15345164-67ab-3e49-f9d6-e29000000012
+15345164-67ab-3e49-f9d6-e29000000034
+```
+
+各個合法卡號分別獨立一行，在跑 scanner.py 時 Pi 即可載入合法卡號的清單以進行判斷
